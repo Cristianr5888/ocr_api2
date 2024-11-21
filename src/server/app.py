adc7d0e7a598e78c0fd8b1ac6474fbeeb94e0eae
Cristian Jimenez <cristian.vanced1858@gmail.com>
2024-11-21 14:28:53 -0700

    diccionary added

--- src/server/app.py
+++ src/server/app.py
@@ -10,22 +10,67 @@ CORS(app)
 
 ocr = PaddleOCR(use_angle_cls=True, lang='es')
 
-palabras_ignoradas = ["INSTITUTO", "NACIONAL", "ELECTORAL", "MEXICO", "CREDENCIAL", "VOTAR", "SEXO", "FECHADENACIMIENTO"]
+# Lista de palabras ignoradas
+palabras_ignoradas = [
+     "INSTITUTO",
+    "NACIONAL",
+    "ELECTORAL",
+    "NOMBRE",
+    "DOMICILIO",
+    "MEXICO",
+    "CREDENCIAL",
+    "VOTAR",
+    "SEXO",
+    "ANO DE REGISTRO",
+    "SECCION",
+    "VIGENCIA",
+    "MUNICIPIO",
+    "LOCALIDAD",
+    "ESTADO",
+    "EMISION",
+    "VIGENCIA",
+    "REGISTRO",
+    "FEDERAL",
+    "ELECTORES",
+    "REGISTRO FEDERALDE ELECTORES",
+
+]
+
+# Diccionario para corregir apellidos
+diccionario_ñ = {
+    "ANO": "AÑO", "NUNEZ": "NUÑEZ", "ZUNIGA": "ZUÑIGA", 
+    "MUNOZ": "MUÑOZ", "IBANEZ": "IBAÑEZ", "CASTANO": "CASTAÑO", 
+    "MUNIZ": "MUÑIZ", "PENA": "PEÑA", "PINA": "PIÑA", 
+    "OCANA": "OCAÑA", "CASTANEDA": "CASTAÑEDA", "CARRENO": "CARREÑO"
+}
+
+def corregir_texto(texto):
+    """
+    Corrige palabras específicas en base al diccionario_ñ.
+    """
+    palabras = texto.split()
+    palabras_corregidas = [
+        diccionario_ñ.get(palabra.upper(), palabra) for palabra in palabras
+    ]
+    return " ".join(palabras_corregidas)
 
 @app.route('/ocr', methods=['POST'])
 def ocr_imagen():
     if 'file' not in request.files:
         return "No file part", 400
+
     file = request.files['file']
     img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
     results = ocr.ocr(img, cls=True)
 
-    threshold = 0.15
+    threshold = 0.15  # Umbral de confianza mínimo
     resultado_corregido = []
     for line in results[0]:
         text, confidence = line[1][0], line[1][1]
+        # Filtrar palabras ignoradas y corregir texto
         if confidence > threshold and not any(palabra in text.upper() for palabra in palabras_ignoradas):
-            resultado_corregido.append({"text": text, "confidence": confidence})
+            texto_corregido = corregir_texto(text)
+            resultado_corregido.append({"text": texto_corregido, "confidence": confidence})
 
     return jsonify(resultado_corregido)
 
